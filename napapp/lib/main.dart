13e1e2e20d74bb57c23e11471ae4f44f52ba690f
cinderdodo <146771579+cinderdodo@users.noreply.github.com>
2026-06-25 02:22:18 +0200

    rimosso pacchetto calendar view dal main

--- napapp/lib/main.dart
+++ napapp/lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:calendar_view/calendar_view.dart';
 import 'theme/util.dart';
 import 'theme/theme.dart';
 import 'screens/home_page.dart';
 import 'services/notification_service.dart';
 
 void main() async {
-  // alarm notification initialization
   WidgetsFlutterBinding.ensureInitialized();
   await NotificationService.init();
   runApp(const MyApp());
@@ -15,25 +13,18 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // event controller for alarms
-  static final EventController _eventController = EventController();
-
   @override
   Widget build(BuildContext context) {
     final textTheme = createTextTheme(context, "Noto Sans", "Noto Sans");
     final theme = MaterialTheme(textTheme);
-    // provider
-    return CalendarControllerProvider(
-      controller: _eventController,
-      child: MaterialApp(
-        title: "Nap App",
-        debugShowCheckedModeBanner: false,
-        theme: theme.light(),
-        darkTheme: theme.dark(),
-        themeMode: ThemeMode.light,
-        home: const HomePage(),
-        routes: {'/homepage': (context) => const HomePage()},
-      ),
+    return MaterialApp(
+      title: "Nap App",
+      debugShowCheckedModeBanner: false,
+      theme: theme.light(),
+      darkTheme: theme.dark(),
+      themeMode: ThemeMode.light,
+      home: const HomePage(),
+      routes: {'/homepage': (context) => const HomePage()},
     );
   }
 }
